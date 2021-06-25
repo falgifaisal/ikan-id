389a5deeb7183da939c4a3b331b3de273e7be250
Danang Eko Alfianto <danangekal@gmail.com>
2021-06-25 21:34:05 +0700

    perf: update all deps (next 11) & relative path, etc

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,6 @@
-import {
-  ReactElement, useEffect, useLayoutEffect, memo,
-} from 'react';
+import { ReactElement, useEffect, useLayoutEffect, memo } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { useAppContext } from 'context/app-context';
 import styles from './header.module.scss';
@@ -29,19 +28,20 @@ function Header(): ReactElement {
         }`}
       >
         <div className="container">
-          <a
-            href="/"
-            title="Home"
-            className={`${styles.navbarBrand} navbar-brand rounded`}
-          >
-            <Image
-              alt="Brand logo"
-              src="/fish.png"
-              width={50}
-              height={32}
-              priority
-            />
-          </a>
+          <Link href="/">
+            <a
+              title="Home"
+              className={`${styles.navbarBrand} navbar-brand rounded`}
+            >
+              <Image
+                alt="Brand logo"
+                src="/fish.png"
+                width={50}
+                height={32}
+                priority
+              />
+            </a>
+          </Link>
           <h2 className="text-info mb-0">Ikan-ID</h2>
           <div>
             <ul className="navbar-nav mr-auto">
@@ -64,8 +64,7 @@ function Header(): ReactElement {
                       d="M15.854 5.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708L12.5 7.793l2.646-2.647a.5.5 0 0 1 .708 0z"
                     />
                     <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-                  </svg>
-                  {' '}
+                  </svg>{' '}
                   {isAdmin ? (
                     <span className="text-info small">(Admin)</span>
                   ) : (
--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,4 +1,5 @@
 import { ReactElement, memo } from 'react';
+import Link from 'next/link';
 
 import { useAppContext } from 'context/app-context';
 import { createArrayNumber } from 'utils/common';
@@ -14,29 +15,32 @@ function Pagination(): ReactElement {
       <nav>
         <ul className="pagination justify-content-center">
           <li className="page-item disabled">
-            <a className="page-link" href="/" tabIndex={-1}>
-              Previous
-            </a>
+            <Link href="/">
+              <a className="page-link" tabIndex={-1}>
+                Previous
+              </a>
+            </Link>
           </li>
-          {dataPage?.length
-            && dataPage.map((val: any) => (
+          {dataPage?.length &&
+            dataPage.map((val: any) => (
               <li
                 key={val}
                 className={`page-item ${val === defaultPage ? 'active' : ''}`}
               >
-                <a
-                  className="page-link"
-                  href="/"
-                  onClick={() => setState({ page: val })}
-                >
-                  {val}
-                </a>
+                <Link href="/">
+                  <a
+                    className="page-link"
+                    onClick={() => setState({ page: val })}
+                  >
+                    {val}
+                  </a>
+                </Link>
               </li>
             ))}
           <li className="page-item">
-            <a className="page-link" href="/">
-              Next
-            </a>
+            <Link href="/">
+              <a className="page-link">Next</a>
+            </Link>
           </li>
         </ul>
       </nav>
